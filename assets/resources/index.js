662c0ac73c14f0b87986446235d0e9c751ecba64
mcl0vin34 <8ug8ear8ounce@gmail.com>
2024-12-11 16:10:35 +0500

    back to backup

--- assets/resources/index.js
+++ assets/resources/index.js
@@ -1,8 +1,4 @@
-System.register("chunks:///_virtual/index.js",["./cjs-loader.mjs"],(function(e,t){var r;return{setters:[function(e){r=e.default}],execute:function(){e("default",void 0);var u=e("__cjsMetaURL",t.meta.url);r.define(u,(function(t,r,u,n,s){var i,c;System.register("chunks:///_virtual/resources",[],(function(){return{execute:function(){}}})),i="virtual:///prerequisite-imports/resources",c="chunks:///_virtual/resources",System.register(i,[c],(function(e,t){return{setters:[function(t){var r={};for(var u in t)"default"!==u&&"__esModule"!==u&&(r[u]=t[u]);e(r)}],execute:function(){}}})),e("default",u.exports)}),{})}}}));
-
-System.register("chunks:///_virtual/index.mjs_cjs=&original=.js",["./index.js","./cjs-loader.mjs"],(function(e,t){var r,n;return{setters:[function(t){r=t.__cjsMetaURL;var n={};n.__cjsMetaURL=t.__cjsMetaURL,n.default=t.default,e(n)},function(e){n=e.default}],execute:function(){r||n.throwInvalidWrapper("./index.js",t.meta.url),n.require(r)}}}));
-
-System.register("chunks:///_virtual/resources",["./index.mjs_cjs=&original=.js"],(function(){return{setters:[null],execute:function(){}}}));
+System.register("chunks:///_virtual/resources",[],(function(){return{execute:function(){}}}));
 
 (function(r) {
   r('virtual:///prerequisite-imports/resources', 'chunks:///_virtual/resources'); 
